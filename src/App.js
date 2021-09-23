47e96b17b917a99a576d83131440e819d9157699
Alena Buiko <abuiko.ma@gmail.com>
2021-09-23 18:47:53 -0400

    add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Nav from "./Nav";
+import About from "./About";
+import Shop from "./Shop";
 
 function App() {
   return (
     <div className="App">
-
+      <Nav />
+      <About />
+      <Shop />
     </div>
   );
 }
